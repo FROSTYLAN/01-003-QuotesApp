c457d84753a82e01f1de8f3cbfa2ac4cdc574780
FROSTYLAN <castillo089frosty@gmail.com>
2022-02-26 00:32:15 -0500

    Array de colores y funcion de alternarlo en el fondo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div>
+  
+  const colors = 
+  [
+    "#845EC2", 
+    "#2C73D2", 
+    "#0081CF", 
+    "#0089BA", 
+    "#008E9B", 
+    "#008F7A", 
+    "#B39CD0", 
+    "#FBEAFF", 
+    "#00C9A7", 
+    "#D65DB1", 
+    "#FF6F91",
+    "#FF9671", 
+    "#FFC75F",
+    "#F9F871"
+  ]
+  const getColor = () => colors[Math.floor(Math.random() * colors.length)];
+  const [alterColor, setAlterColor] = useState(getColor());
+  const changeColor = () => {
+    setAlterColor(getColor());
+  }
 
+  return (
+    <div className='app' style={{background: alterColor}}>
+      <button onClick={changeColor}></button>
     </div>
   );
 }
